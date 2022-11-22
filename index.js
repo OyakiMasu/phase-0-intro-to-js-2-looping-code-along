c4d10fd9a265f837597106d75ce844eba168b4c3
OyakMasu21 <akzaki17@gmail.com>
2022-11-22 18:06:16 +0300

    Completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+//for (let age = 30; age < 40; age++) {
+  //  console.log(`I'm ${age} years old. Happy birthday to me!`);
+    //debugger;
+//}
+/*const names = [Guadalupe, Ollie, Aki]
+function writeCards(names, event) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Thank you, ${names[i]} for the wonderful ${event} gift!`);
+  return names;
+  }
+}
+*/
+const names = ["Guadalupe,", ",OLlie,", ",Aki"]
+function writeCards(name, event) {
+    const array = []
+    for (let i = 0; i < name.length; i++) {
+        array.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return array;
+}
+function countDown(number) {
+    while (number > 0) {
+        console.log(number);
+        number -= 1;
+    }
+    console.log(number);
+}
